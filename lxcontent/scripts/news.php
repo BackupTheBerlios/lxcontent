6ab0c6b26734aaca4073d61bdd0a4b61cd8c2236
cypher <cypher>
2005-04-21 03:49:12 +0000

    Added the possibility to put some text before and after news articles.

--- lxcontent/scripts/news.php
+++ lxcontent/scripts/news.php
@@ -19,18 +19,23 @@
 		
 		$content['articles']= "";
 		
-		if (!isset($_GET['page']) and $xmlParser->match("/content[1]/welcome_text[1]"))
+		if (!isset($_GET['page']) and $xmlParser->match("/content[1]/header[1]"))
 		{
-			$content['welcome_text'] .= "<div class=\"welcome_text\">\n" . stripTag("text", $xmlParser->exportAsXml("/content[1]/welcome_text[1]")) . "\n</div>\n";
+			$content['header'] .= "<div class=\"news_header\">\n" . stripTag("header", $xmlParser->exportAsXml("/content[1]/header[1]")) . "\n</div>\n";
+		}
+		
+		if (!isset($_GET['page']) and $xmlParser->match("/content[1]/footer[1]"))
+		{
+			$content['footer'] .= "<div class=\"news_footer\">\n" . stripTag("footer", $xmlParser->exportAsXml("/content[1]/footer[1]")) . "\n</div>\n";
 		}
 		
 		$i = ($page-1)*$conf['articles_per_site']+1;
-		while (($i < $page+$conf['articles_per_site']) and $xmlParser->match("/content[1]/article[$i]"))
+		while (($i < $page+$conf['articles_per_site']) and $xmlParser->match("/content[1]/news[1]/article[$i]"))
 		{
-			$news['author'] = $xmlParser->getData("/content[1]/article[$i]/author[1]");
-			$news['date'] = $xmlParser->getData("/content[1]/article[$i]/date[1]");
-			$news['title'] = $xmlParser->getData("/content[1]/article[$i]/title[1]");
-			$news['text'] = stripTag("article", $xmlParser->exportAsXml("/content[1]/article[$i]/text[1]"));
+			$news['author'] = $xmlParser->getData("/content[1]/news[1]/article[$i]/author[1]");
+			$news['date'] = $xmlParser->getData("/content[1]/news[1]/article[$i]/date[1]");
+			$news['title'] = $xmlParser->getData("/content[1]/news[1]/article[$i]/title[1]");
+			$news['text'] = stripTag("text", $xmlParser->exportAsXml("/content[1]/news[1]/article[$i]/text[1]"));
 			$content['articles'].= template($news, $conf['article_template']);
 			$i++;
 		}
